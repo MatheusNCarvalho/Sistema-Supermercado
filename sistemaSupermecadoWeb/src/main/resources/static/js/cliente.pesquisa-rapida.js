88171581de22d35671db7718ea04018111725e1f
Arcelz <lucaslucasfabio@hotmail.com>
2016-11-14 22:06:30 -0200

    colocando idFornecedor

--- sistemaSupermecadoWeb/src/main/resources/static/js/cliente.pesquisa-rapida.js
+++ sistemaSupermecadoWeb/src/main/resources/static/js/cliente.pesquisa-rapida.js
@@ -71,7 +71,7 @@ Brewer.TabelaClientePesquisaRapida = (function() {
 	
 		var clienteSelecionado = $(evento.currentTarget);
 		$('#nomeCliente').val(clienteSelecionado.data('nome'));
-		$('#codigoCliente').val(clienteSelecionado.data('codigo'));
+		$('#codigoCliente').val(clienteSelecionado.data('idFornecedor'));
 	}
 	
 	return TabelaClientePesquisaRapida;
