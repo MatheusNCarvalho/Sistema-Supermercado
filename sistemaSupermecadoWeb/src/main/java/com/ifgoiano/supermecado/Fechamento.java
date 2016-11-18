8d75b7d78c0b16eef2a6599c3567bcd3063ac0a2
inacioedson <inacioedson@hotmail.com>
2016-11-18 19:27:19 -0200

    Fechamento repositório

---
+++ sistemaSupermecadoWeb/src/main/java/com/ifgoiano/supermecado/Fechamento.java
@@ -0,0 +1,8 @@
+package com.ifgoiano.supermecado.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ifgoiano.supermecado.model.Fechamentos;
+
+public interface Fechamento extends JpaRepository<Fechamentos, Long>{
+
+}
