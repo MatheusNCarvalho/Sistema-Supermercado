1afeaedba2137bdfcfa179260414d0592c786d1a
MatheusNCarvalho <matheusnevesdecarvalho@gmail.com>
2016-11-19 15:14:27 -0200

    Addd configuração no pacote confg

--- sistemaSupermecadoWeb/src/main/java/com/ifgoiano/supermecado/config/SecurityConfig.java
+++ sistemaSupermecadoWeb/src/main/java/com/ifgoiano/supermecado/config/SecurityConfig.java
@@ -3,10 +3,51 @@
  */
 package com.ifgoiano.supermecado.config;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 /**
  * @author brunn
  *
  */
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("joao").password("joao").roles("CADASTRAR_VINHO").and()
+			.withUser("maria").password("maria").roles("CADASTRAR_VINHO", "LISTAR_VINHO").and()
+			.withUser("admin").password("admin").roles("CADASTRAR_VINHO", "LISTAR_VINHO", "LISTAR_FORNECEDORES");
+	}
 
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+			.antMatchers("/layout/**");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/produtos/novo").hasRole("CADASTRAR_VINHO")
+				.antMatchers("/produtos/**").hasRole("LISTAR_VINHO")
+				.antMatchers("/fornecedores/**").hasRole("LISTAR_FORNECEDORES")
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+	}
+	
+	
+	
+	
 }
