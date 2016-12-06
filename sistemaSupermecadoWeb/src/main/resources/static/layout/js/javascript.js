208ebe627697e7956e425b03026fdb706c80dd42
Lucas Gonçalves <lucaslucasfabio@hotmail.com>
2016-12-06 14:05:20 -0200

    Organizando botos para fechamento do caixa e movimentp

--- sistemaSupermecadoWeb/src/main/resources/static/layout/js/javascript.js
+++ sistemaSupermecadoWeb/src/main/resources/static/layout/js/javascript.js
@@ -9,9 +9,24 @@ $('#confirmacaoExclusaoModal').on('show.bs.modal', function(event) {
 	let action = button.data('action');
 	if (!action.endsWith('/')) {
 		action += '/';
-	};
+	}
 	form.attr('action', action + codigoTitulo);
 	modal.find('.modal-body span').html('Tem certeza que deseja excluir o título <strong>' + descricaoTitulo + '</strong>?');
+	
+});
+$('#modalMovimentoCaixa').on('show.bs.modal',function(event){
+	let button = $(event.relatedTarget);
+	let nomeModal = button.data('id');
+	alert(nomeModal)
+	
+	let modal=$(this);
+	let form = modal.find('form');
+	let action = button.data('action');
+	if(!action.endsWith('/')){
+		action+='/';
+	}
+
+	$('.modal-title').text("asdsadasdasdsda").fadeIn()
 	
 });
 
